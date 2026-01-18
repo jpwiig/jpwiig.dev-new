1174e476be918761a4e6575b2507c7b59cd523c0
jpwiig <jpwiig@gmail.com>
2026-01-18 21:54:33 +0100

    testing somthing silly

--- js/badge/wiigen-badge.js
+++ js/badge/wiigen-badge.js
@@ -3,6 +3,7 @@ class Wiigen extends HTMLElement {
         super();
     } 
     connectedCallback() {
+        alert("hallaien")
         const shadow = this.attachShadow({mode: "open"});
         const anchor = document.createElement("a");
         anchor.setAttribute("class", "bagde")
